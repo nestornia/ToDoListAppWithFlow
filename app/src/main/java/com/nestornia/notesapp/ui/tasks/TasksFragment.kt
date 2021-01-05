f8d8a3d103dc6350e2951a018fa51d9615f705d4
nestornia <nestor.nia@gmail.com>
2021-01-04 21:33:32 -0800

    Implementing the recyclerView.

--- app/src/main/java/com/nestornia/notesapp/ui/tasks/TasksFragment.kt
+++ app/src/main/java/com/nestornia/notesapp/ui/tasks/TasksFragment.kt
@@ -1,8 +1,13 @@
 package com.nestornia.notesapp.ui.tasks
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.nestornia.notesapp.R
+import com.nestornia.notesapp.databinding.FragmentTasksBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,4 +15,24 @@ class TasksFragment : Fragment(R.layout.fragment_tasks) {
 
     private val viewModel: TasksViewModel by viewModels()
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val binding = FragmentTasksBinding.bind(view)
+
+        val taskAdapter = TaskAdapter()
+
+        binding.apply {
+            rvTasks.apply {
+                adapter = taskAdapter
+//                layoutManager = LinearLayoutManager(context)
+                layoutManager = LinearLayoutManager(requireContext())
+                setHasFixedSize(true)
+            }
+        }
+
+        viewModel.tasks.observe(viewLifecycleOwner, {
+            taskAdapter.submitList(it)
+        })
+    }
 }
--- app/src/main/java/com/nestornia/notesapp/ui/tasks/TasksViewModel.kt
+++ app/src/main/java/com/nestornia/notesapp/ui/tasks/TasksViewModel.kt
@@ -2,9 +2,12 @@ package com.nestornia.notesapp.ui.tasks
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
 import com.nestornia.notesapp.data.TaskDao
 
 class TasksViewModel @ViewModelInject constructor(
     private val taskDao: TaskDao
 ) : ViewModel() {
+
+    val tasks = taskDao.getTasks().asLiveData()
 }
